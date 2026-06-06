12cfabadb9e363e14d7dcb936ddc543e89ad027b
Jeca <smsacha2013@yandex.ru>
2026-06-06 19:17:47 +0700

    Fix возвращать exit 1 при ошибке запуска, ADD новые команды

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import sys
 import argparse
 from pathlib import Path
 
-from src.service.scaner import scan, show_index, show_history
+from src.service.reports import show_index, show_history, show_duplicates
+from src.service.scaner import scan
 from src.database.db import connect
 from src.config import DB_PATH
 from src.logs_service.logs import setup_logging
@@ -11,7 +12,8 @@ from src.logs_service.logs import setup_logging
 def build_parser():
     parser = argparse.ArgumentParser(prog="indexer", description="Индексатор папок (SQLite).")
     parser.add_argument("--db", default=str(DB_PATH), help="файл базы SQLite")
-    parser.add_argument("-v", "--verbose", action="store_true", help="подробный лог")
+    parser.add_argument("-v", "--verbose", action="store_true", help="INFO лог ")
+    parser.add_argument("-vv", "--vverbose", action="store_true", help="DEBUG лог")
     sub = parser.add_subparsers(dest="command", required=True)
 
     p = sub.add_parser("scan", help="просканировать папку")
@@ -24,11 +26,14 @@ def build_parser():
 
     p = sub.add_parser("history", help="история запусков")
     p.add_argument("path")
+
+    p = sub.add_parser("duplicates", help="найти дубликаты файлов")
+    p.add_argument("path")
     return parser
 
-def main(argv=None):
+def main(argv = None):
     args = build_parser().parse_args(argv)
-    setup_logging(args.verbose)
+    setup_logging(args.verbose, args.vverbose)
     with connect(Path(args.db)) as conn:
         try:
             if args.command == "scan":
@@ -48,8 +53,14 @@ def main(argv=None):
                     conn=conn,
                     path=args.path,
                 )
+            elif args.command == "duplicates":
+                show_duplicates(
+                    conn=conn,
+                    path=args.path,
+                )
         except Exception:
             logging.exception("Ошибка запуска")
+            return 1
 
 if __name__ == '__main__':
     sys.exit(main())
